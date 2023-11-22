1ee961aee2c33c6680628cfff4f7b405dc06c65b
JoelChiaraviglio <chiaravigliobrianjoel@gmail.com>
2023-11-21 22:04:50 -0300

    WEB

---
+++ ProyectoFinal.WEB/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div style="padding: 3rem;">
+    <RouterView />
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ ProyectoFinal.WEB/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+import {ref, onMounted} from 'vue';
+
+// import Card from 'primevue/card'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+
+let candidateList = ref([])
+
+
+const getCandidateList = async () => {
+var response = await fetch('https://localhost:7174/api/candidates')
+const data = await response.json()
+candidateList.value = data.candidateList
+}
+
+
+
+onMounted(async () => {
+  await getCandidateList();
+})
+
+</script>
+
+<template>
+    <div class="grid">
+      <div class="col-12">
+          <DataTable :value="candidateList" >
+            <Column field="id" header="#"></Column>
+            <Column field="firstName" header="Nombre"></Column>
+            <Column field="lastName" header="Apellido"></Column>
+
+          </DataTable>
+
+      </div>
+    </div>
+</template>
+
